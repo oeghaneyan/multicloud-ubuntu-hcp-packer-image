2173fe7cbf321bff4bbb037533ec091cc87d228b
omid.eghaneyan <oeghaneyan@gmail.com>
2022-09-27 09:39:40 -0700

    Changed role to CIS Ubuntu 20.04 Ansible

--- post-script.sh
+++ post-script.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-# Update packages
-#sudo apt-get update -y
-
-# Install Ansible
-#sudo apt install software-properties-common -y
-#sudo apt-add-repository ppa:ansible/ansible
-#sudo apt-get update -y
-#sudo apt-get install ansible -y
-
-# Pull down Ansible Playbooks
-#git clone https://github.com/omeeomi/multicloud-ubuntu-hcp-packer-image.git
+echo "post-script.sh is an empty placeholder script - No action taken."
 
 #ls
-echo "post-script.sh is an empty placeholder script - No action taken."
+
